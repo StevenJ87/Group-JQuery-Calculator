a05443a8fec23020b515a5f064c2faeca18ff01f
BertJ401 <bertjohnson401@gmail.com>
2020-06-24 11:24:02 -0500

    equal is commented out cuz broke

--- script.js
+++ script.js
@@ -1,56 +1,86 @@
+let buildNum = "";
+let currentLine = document.querySelector("#first-number");
+let secondLine = document.querySelector("#second-number");
+let operLine = document.querySelector("#operator");
+let resultLine= document.querySelector("#result");
+let operOne="";
+let operTwo="";
+let operation="";
+
+
+
+console.log(currentLine)
+
 $(document).ready(function() {
-    $("#button-1").click(function(){
-      
+
+     
+        
+    $(".btn-primary").click(function(){
+    var number = $(this).val();
+     buildNum=buildNum.toString()+number.toString();
+     currentLine.innerText=buildNum;
+     return buildNum;
     });
-    $("#button-2").click(function(){
-      
-    });
-    $("#button-3").click(function(){
-      
-    });
-    $("#button-4").click(function(){
-      
-    });
-    $("#button-5").click(function(){
-      
-    });
-    $("#button-6").click(function(){
-      
-    });
-    $("#button-7").click(function(){
-      
-    });
-    $("#button-8").click(function(){
-      
-    });
-    $("#button-9").click(function(){
-      
-    });
-    $("#button-0").click(function(){
-      
-    });
+
     $("#button-plus").click(function(){
-      
+        operOne=buildNum;
+        buildNum="";
+        operation="+";
+        operator.innerText=operation;
+        currentLine=secondLine
+
     });
     $("#button-minus").click(function(){
-      
+        operOne=buildNum;
+        buildNum="";
+        operation="-";
+        operator.innerText=operation;
+        currentLine=secondLine
+
     });
     $("#button-multiply").click(function(){
-      
+        operOne=buildNum;
+        buildNum="";
+        operation="*";
+        operator.innerText=operation;
+        currentLine=secondLine
+
     });
     $("#button-divide").click(function(){
-      
+        operOne=buildNum;
+        buildNum="";
+        operation="/";
+        operator.innerText=operation;
+        currentLine=secondLine
+
     });
     $("#button-power").click(function(){
-      
+        operOne=buildNum;
+        buildNum="";
+        operation="^";
+        operator.innerText=operation;
+        currentLine=secondLine
+
     });
-    $("#button-equal").click(function(){
-      
-    });
+    // $("#button-equal").click(function(){
+    //   operTwo=buildNum;
+    //   if (operation == "+") {
+    //       outPut=operOne.valueOf() + operTwo.valueOf();
+    //       resultLine.innerText=outPut.toString();
+    //   else if {}
+
+    //   }
+    // });
     $("#button-clear").click(function(){
-      
+      operOne="";
+      operTwo=""; 
+      operator.innerText="";
+      currentLine = document.querySelector("#first-number");
+      currentLine.innerText="";
+      secondLine.innerText="";
     });
     
     
     });
-    
+
+ 
