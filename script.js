3ae450b6f118b281e88984ce42cc4c20545c95dc
Courtney Jones <courtneyrjones95@gmail.com>
2020-06-24 10:08:07 -0500

    made seperate js file

---
+++ script.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    $("#button-1").click(function(){
+      
+    });
+    $("#button-2").click(function(){
+      
+    });
+    $("#button-3").click(function(){
+      
+    });
+    $("#button-4").click(function(){
+      
+    });
+    $("#button-5").click(function(){
+      
+    });
+    $("#button-6").click(function(){
+      
+    });
+    $("#button-7").click(function(){
+      
+    });
+    $("#button-8").click(function(){
+      
+    });
+    $("#button-9").click(function(){
+      
+    });
+    $("#button-0").click(function(){
+      
+    });
+    $("#button-plus").click(function(){
+      
+    });
+    $("#button-minus").click(function(){
+      
+    });
+    $("#button-multiply").click(function(){
+      
+    });
+    $("#button-divide").click(function(){
+      
+    });
+    $("#button-power").click(function(){
+      
+    });
+    $("#button-equal").click(function(){
+      
+    });
+    $("#button-clear").click(function(){
+      
+    });
+    
+    
+    });
+    
